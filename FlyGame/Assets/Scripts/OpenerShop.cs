90317d7a110be75cc7459d1670aa8f218a76a249
Vlad <88041483+MaryDayGit@users.noreply.github.com>
2022-09-16 11:56:22 +0300

    add anim open/close shop

---
+++ FlyGame/Assets/Scripts/OpenerShop.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenerShop : MonoBehaviour
+{
+    public GameObject panel;
+    public void OpenPanel()
+    {
+        if (panel != null)
+        {
+            Animator animator = panel.GetComponent<Animator>();
+            if (animator != null)
+            {
+                bool isOpen = animator.GetBool("OpenMenuTrigger");
+                animator.SetBool("OpenMenuTrigger", !isOpen);
+            }
+
+        }
+    }
+}
