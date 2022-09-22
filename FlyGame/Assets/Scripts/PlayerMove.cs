2c5b877f7938339567c4e2ea63f0936ad47472e3
Vlad <88041483+MaryDayGit@users.noreply.github.com>
2022-09-22 11:30:36 +0300

    Fix move

--- FlyGame/Assets/Scripts/PlayerMove.cs
+++ FlyGame/Assets/Scripts/PlayerMove.cs
@@ -6,14 +6,26 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public GameObject projectilePrefab;
+    public float timeBetweenShots = 0.01f;
+    private float timestamp;
     // Update is called once per frame
     void Update()
     {
 
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            if (Time.time >= timestamp)
+            {
+                Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+                timestamp = Time.time + timeBetweenShots;
+                Debug.Log(Time.time);
+                Debug.Log(timestamp);
+            }
+        }
     }
-    void OnMouseDown()
+    private void Start()
     {
-        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        DontDestroyOnLoad(gameObject);
     }
 
 }
